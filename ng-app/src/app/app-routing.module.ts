bed8eb3abe34a668f657f0483938fd17caf09ae3
Chartchai Doungsa-ard <chartchai.d@cmu.ac.th>
2017-04-21 08:19:29 +0700

    first commit

---
+++ ng-app/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import {NgModule}              from '@angular/core';
+import {RouterModule, Routes}  from '@angular/router';
+import {FileNotFoundComponent} from "./filenotfound/file-not-found.component";
+const appRoutes: Routes = [
+
+  {
+    path: '',
+    redirectTo: '/list',
+    pathMatch: 'full'
+  },
+  {path: '**', component: FileNotFoundComponent},
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {
+}
